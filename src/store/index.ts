a3d60b9ddc0da6c27cadc26cb4822ca760234254
Angela Caldas <aes.caldas@gmail.com>
2023-05-25 15:22:08 -0300

    chore: create Store with Vuex

---
+++ src/store/index.ts
@@ -0,0 +1,33 @@
+import { InjectionKey } from 'vue';
+import { createStore, Store } from 'vuex';
+import IProject from '@/interfaces/IProject';
+
+
+interface State {
+  projects: IProject[];
+}
+
+// Creating an access key for the store:
+// In case Symbol shows as an error, include in `tsconfig.json`, the lib ["ES2015"]
+export const key: InjectionKey<Store<State>> = Symbol();
+
+// Global states
+export const store = createStore<State>({
+  state: {
+    // Projects list
+    projects: [
+      {
+        id: new Date().toISOString(),
+        name: 'TypeScript'
+      },
+      {
+        id: new Date().toISOString(),
+        name: 'Vue'
+      },
+      {
+        id: new Date().toISOString(),
+        name: 'Vuex'
+      },
+    ]
+  }
+});
